b43725ad64309224356e15fb25cdd4e7d9444aad
Arik92 <arik92@gmail.com>
2017-02-16 11:14:09 +0200

    modular branch of master

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-var beers=[];
-var addBeer=function(name, category, rating){
-  var b={
-    name: name,
-    category:category,
-    rating: rating
-  };
-  beers.push(b);
-};//addBeer
+
+var beerReviewApp = function() {
+  var beers=[];
+  var addBeer=function(name, category, rating){
+    var b={
+      name: name,
+      category:category,
+      rating: rating
+    };
+    beers.push(b);
+  };//addBeer
+  var updateBeers = function(){
+    $('ul li').remove();
+    for (var i = 0;i < beers.length;i++){
+      $('.beers-list').append('<li>Name: ' + beers[i].name + ' Category: '
+      +beers[i].category+' Rating: '+beers[i].rating+'</li>');
+    }//for
+  };//updateBeers
+
+  return {
+    addBeer: addBeer,
+    updateBeers: updateBeers
+  }
+}//app
+var app=beerReviewApp();
 $('.post-beer').on('click', function(){
   var name = $('.beer-input').val();
   var cat = $('.category-input').val();
   var rate = $('#select').val();
-  addBeer(name,cat,rate);
-  updateBeers();
+  app.addBeer(name,cat,rate);
+  app.updateBeers();
 });// event handler for  POST button
 $('#sort').on('click',function(){
   beers.sort(function(a, b) {
@@ -27,10 +43,3 @@ $('#rev').on('click',function(){
   beers.reverse();
   updateBeers();
 });
-var updateBeers = function(){
-  $('ul li').remove();
-  for (var i = 0;i < beers.length;i++){
-    $('.beers-list').append('<li>Name: ' + beers[i].name + ' Category: '
-    +beers[i].category+' Rating: '+beers[i].rating+'</li>');
-  }//for
-};//updateBeers
